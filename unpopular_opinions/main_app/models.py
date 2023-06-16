c0cd16f8c73304b2f70d65b34a86b5626fda6d0a
Luke Hufton <lukehufton@gmail.com>
2023-06-16 13:32:27 -0500

    models1

--- unpopular_opinions/main_app/models.py
+++ unpopular_opinions/main_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+RATINGS = (
+    ("Adults only", "NC-17"),
+    ("Restricted", "R"),
+    ("Parents Strongly Cautioned", "PG-13"),
+    ("Parental Guidance", "PG"),
+    ("Parental Guidance", "PG"),
+)
+
+
+class Movie(models.model):
+    RATINGS
+    title = models.CharField(max_length=100)
+    rating = models.CharField()
+
+
+class Opinion(models.model):
+    pass
+
+
+class Comment(models.model):
+    pass
+
+
+class Opinion(models.model):
+    pass
