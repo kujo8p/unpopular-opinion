8d551465f6a9f623e79fa7d28c71d7d038ab9e6e
Luke Hufton <lukehufton@gmail.com>
2023-06-16 14:48:21 -0500

    power outtage save

--- unpopular_opinions/main_app/models.py
+++ unpopular_opinions/main_app/models.py
@@ -13,16 +13,20 @@ RATINGS = (
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    rating = models.CharField(max_length=200)
+    rating = models.CharField(max_length=5)
 
 
 class Opinion(models.Model):
-    pass
+  title = models.CharField(max_length=150)
+  content = models.CharField(max_length=500)
+  release_year = 
 
 
 class Comment(models.Model):
     pass
 
 
-class Opinion(models.Model):
+class Personnel(models.Model):
+  role = models.CharField(max_length=50)
+  name = models.CharField(max_length=100)
     pass
