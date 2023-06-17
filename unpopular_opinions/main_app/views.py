919e682cba1fdf5a7bcb5743ae8206ecce19b371
Luke Hufton <lukehufton@gmail.com>
2023-06-17 16:40:25 -0500

    updated views

--- unpopular_opinions/main_app/views.py
+++ unpopular_opinions/main_app/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+import os
+import uuid
+import boto3
+from django.shortcuts import render, redirect
+from .models import Opinion
+from django.contrib.auth import login
+
+# name possibly subject to change
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
 def home(request):
-  return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, "about.html")
+
+
+# Function names may change when I know what has been happening on the  Front-end.
+def opinions_index(request):
+    opinions = Opinion.objects.filter(user=request.user)
+    return render(request, "opinions/index.html", {"opinions": opinions})
+
+
+def opinions_detail(request):
+    opinion = Opinion.objects.get(id=opinion_id)
