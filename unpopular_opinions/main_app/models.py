b94a324ddfcd066bf375b6a74beee34f2aa95063
Luke Hufton <lukehufton@gmail.com>
2023-06-17 22:30:03 -0500

    merge

--- unpopular_opinions/main_app/models.py
+++ unpopular_opinions/main_app/models.py
@@ -32,11 +32,10 @@ class Comment(models.Model):
 
 
 class Opinion(models.Model):
-    author = models.CharField(max_length=100)
-    title = models.CharField(max_length=150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tldr = models.CharField(max_length=150)
     date = models.DateField("posted on")
-    content = models.CharField(max_length=500)
+    content = models.CharField(max_length=10000)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     personnel = models.ForeignKey(Personnel, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
