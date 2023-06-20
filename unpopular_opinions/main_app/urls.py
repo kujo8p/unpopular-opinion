ca72f58c88b119ddcd0c6ff6e7f1c38b7fddc2c7
Kurt <kujo8p@gmail.com>
2023-06-19 20:35:44 -0500

    func

--- unpopular_opinions/main_app/urls.py
+++ unpopular_opinions/main_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("opinion/", views.opinions_index, name="opinion"),
     path("opinion/<int:opinion_id>/", views.opinion_detail, name='opinion_detail'),
     path("movies/", views.movie_index, name="movies"),
+    path("movies/<int:movie_id>/", views.movie_detail, name='movie_detail'),
     path('movies/create/', views.MovieCreate.as_view(), name='movies_create'),
     path('accounts/signup/', views.signup, name='signup'),
 ]
--- unpopular_opinions/main_app/views.py
+++ unpopular_opinions/main_app/views.py
@@ -36,8 +36,14 @@ def opinion_detail(request, opinion_id):
     })
 
 def movie_index(request):
-    movie = Movie.objects.all()
-    return render(request, "movies/index.html", {"movie": movie})
+    movies = Movie.objects.all()
+    return render(request, "movies/index.html", {"movies": movies})
+
+def movie_detail(request, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+    return render(request, 'movies/detail.html', {
+        'movie': movie
+    })
 
 class MovieCreate(CreateView):
     model = Movie
