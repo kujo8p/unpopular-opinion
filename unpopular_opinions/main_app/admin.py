bccbf4c4fa18712a882f3d2968dddb838b7c935c
Kurt <kujo8p@gmail.com>
2023-06-21 15:04:11 -0500

    style

--- unpopular_opinions/main_app/admin.py
+++ unpopular_opinions/main_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Movie, Personnel
+from .models import Movie, Personnel, Opinion, Comment
 
 # Register your models here.
 admin.site.register(Movie)
 admin.site.register(Personnel)
+admin.site.register(Opinion)
+admin.site.register(Comment)
