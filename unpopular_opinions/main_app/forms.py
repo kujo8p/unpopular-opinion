9420bc0fa8a974f67c12e556ea65bce90a2992ee
Daniel Loewenstein <loewenst@gmail.com>
2023-06-21 13:49:38 -0400

    added personnel and personnel detail pages

--- unpopular_opinions/main_app/forms.py
+++ unpopular_opinions/main_app/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm, ModelChoiceField
-from .models import Comment, Opinion, Movie
+from .models import Comment, Opinion, Movie, Personnel
 
 class CommentForm(ModelForm):
   class Meta:
@@ -12,6 +12,17 @@ class OpinionForm(ModelForm):
     super(OpinionForm, self).__init__(*args, **kwargs)
     self.fields['movie_choice'] = ModelChoiceField(queryset=Movie.objects.all())
 
+
   class Meta:
     model = Opinion
-    fields = ['tldr', 'content', 'movie_choice']
+    fields = ['movie_choice', 'tldr', 'content']
+
+class OpinionFormPerson(ModelForm):
+  def __init__(self, *args, **kwargs):
+    super(OpinionForm, self).__init__(*args, **kwargs)
+    self.fields['person_choice'] = ModelChoiceField(queryset=Personnel.objects.all()) 
+    self.fields['person_movie'] = ModelChoiceField(queryset=Personnel.movie_set.all())
+
+  class Meta:
+    model = Opinion
+    fields = ['person_choice', 'person_movie', 'person_role', 'tldr', 'content']
