b990c82e87738fac55bae35ed7440d1ad4b284de
Kurt <kujo8p@gmail.com>
2023-06-22 08:48:59 -0500

    remove check_person

--- unpopular_opinions/main_app/admin.py
+++ unpopular_opinions/main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movie, Personnel
+from .models import Movie, Personnel, Comment, Opinion
 
 # Register your models here.
 admin.site.register(Movie)
