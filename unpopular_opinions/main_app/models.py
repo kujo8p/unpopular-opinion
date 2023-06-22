cd524e01e882beb2acc6d1944210e7ec6cd95093
Kurt <kujo8p@gmail.com>
2023-06-22 16:17:55 -0500

    working

--- unpopular_opinions/main_app/models.py
+++ unpopular_opinions/main_app/models.py
@@ -50,7 +50,7 @@ class Opinion(models.Model):
     tldr = models.CharField('opinion', max_length=150)
     date = models.DateTimeField(auto_now_add=True)
     content = models.CharField('explanation', max_length=10000)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, null=True)
     person = models.ForeignKey(Personnel, on_delete=models.CASCADE, null=True)
     person_role = models.CharField('role', choices=ROLES, default=ROLES[0], null=True)
     person_movie = models.CharField('movie', max_length=150, null=True)
